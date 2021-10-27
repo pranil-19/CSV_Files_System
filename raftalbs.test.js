26b782936fe2500e3cb01af75973b16f8de776fc
ravitejamadala5 <ravitejamadala5@gmail.com>
2021-10-27 14:30:01 +0530

    raftlabs assignment

--- raftalbs.test.js
+++ raftalbs.test.js
@@ -1,21 +1,10 @@
 
 
 // import math file
-const {findBooksAndMagsByAuthor} = require('./raftlabs');
+// const {findBooksAndMag} = require('./raftlabs');
 
-test('Finding a book by isbn', () =>{
-	const result = [];
-	expect(findBookOrMag(books, magazines, "2221-5548-8585")).toEqual(result)
-})
+// test('Finding a book by isbn', () =>{
+// 	const result = [];
+// 	expect(findBookOrMag(books, magazines, "2221-5548-8585")).toEqual(result)
+// })
 
-// describe('raftlabs.js tests', () => {
-//     describe('run.add() Test', () => {
-//         it('should equal 2', () => {
-//             const result = math.add(1, 1);
-//             expect(result).to.equal(2);
-//         });
-//         it('should equal 4', () => {
-//             const result = math.add(2, 2);
-//             expect(result).to.equal(4);
-//         });
-//     });
